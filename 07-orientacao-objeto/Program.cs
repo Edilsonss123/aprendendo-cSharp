9c9c8e0e2dde2540caca913b13c7c45c038af772
root <root@Edilson.localdomain>
2022-08-25 11:08:12 -0300

    Continuação

--- 07-orientacao-objeto/Program.cs
+++ 07-orientacao-objeto/Program.cs
@@ -25,51 +25,51 @@ namespace OrientacaoObjeto
             while (true)
             {
                 
-            string menu = $" ============= Cadastro de cliente ============="+
-            "\n     0 - Sair do cadastro"+
-            "\n     1 - Novo cadastro"+
-            "\n     2 - Listar clientes";
-            Console.WriteLine(menu);
-            var inputEntrada = Console.ReadLine();
-            if (int.TryParse(inputEntrada, out int entrada)) {
-                if (entrada == 0)
-                {
-                    Console.Clear();
-                    break;
-                }
-                else if (entrada == 1)
-                {
-
-                    Console.WriteLine("Cadastro de cliente:\n");
-                    Console.Write("Nome: ");
-                    string Nome = Convert.ToString(Console.ReadLine());
-
-                    Console.Write("CPF: ");
-                    string CPF = Convert.ToString(Console.ReadLine());
-
-                    Console.Write("Telefone: ");
-                    string Telefone = Convert.ToString(Console.ReadLine());
-
-                    (new Usuario(){
-                        Nome = Nome,
-                        Telefone = Telefone,
-                        CPF = CPF
-                    }).Gravar();
-                    Console.Write("Cadastro ralizado com sucesso.\n\n");
-                }
-                else if (entrada == 2)
-                {
-                    List<Cliente> clientes = Cliente.LerClientes();
-                    Console.WriteLine("Lista de clientes\n");
-                    foreach (Cliente c in clientes)
+                string menu = $" ============= Cadastro de cliente ============="+
+                "\n     0 - Sair do cadastro"+
+                "\n     1 - Novo cadastro"+
+                "\n     2 - Listar clientes";
+                Console.WriteLine(menu);
+                var inputEntrada = Console.ReadLine();
+                if (int.TryParse(inputEntrada, out int entrada)) {
+                    if (entrada == 0)
                     {
-                        Console.WriteLine($"Nome: {c.Nome}\nTelefone: {c.Telefone}\nCPF: {c.CPF}");
-                        Console.WriteLine($"\n========================= ========================= =========================\n");
-                    } 
-                }
-                
+                        Console.Clear();
+                        break;
+                    }
+                    else if (entrada == 1)
+                    {
 
-            }
+                        Console.WriteLine("Cadastro de cliente:\n");
+                        Console.Write("Nome: ");
+                        string Nome = Convert.ToString(Console.ReadLine());
+
+                        Console.Write("CPF: ");
+                        string CPF = Convert.ToString(Console.ReadLine());
+
+                        Console.Write("Telefone: ");
+                        string Telefone = Convert.ToString(Console.ReadLine());
+
+                        (new Usuario(){
+                            Nome = Nome,
+                            Telefone = Telefone,
+                            CPF = CPF
+                        }).Gravar();
+                        Console.Write("Cadastro ralizado com sucesso.\n\n");
+                    }
+                    else if (entrada == 2)
+                    {
+                        List<Cliente> clientes = Cliente.LerClientes();
+                        Console.WriteLine("Lista de clientes\n");
+                        foreach (Cliente c in clientes)
+                        {
+                            Console.WriteLine($"Nome: {c.Nome}\nTelefone: {c.Telefone}\nCPF: {c.CPF}");
+                            Console.WriteLine($"\n========================= ========================= =========================\n");
+                        } 
+                    }
+                    
+
+                }
             }
 
         }
